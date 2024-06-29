4bbf17d503b4a629b382163af27c22a4b5505a68
Daniel Caresma Silva <daniel.caresma@gmail.com>
2024-06-29 17:01:18 -0300

    Criando método para deletar livro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -40,6 +40,16 @@ class LivroController {
             res.status(500).json({message: `${erro.message} - Falha na atualização do livro`})
         }
     }
+
+    static async excluirLivro(req, res) {
+        try {
+            const id = req.params.id
+            await livro.findByIdAndDelete(id)
+            res.status(200).json({ message: "livro excluído com sucesso" })
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na exclusão do livro`})
+        }
+    }
 }
 
 export default LivroController
