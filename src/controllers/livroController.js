1a3018a454a87d678bb7be5f7729a6f62d6b06fd
Daniel Caresma Silva <daniel.caresma@gmail.com>
2024-06-29 15:59:53 -0300

    Criando método para listar livro por ID

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js"
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({})
-        res.status(200).json(listaLivros)
+        try {
+            const listaLivros = await livro.find({})
+            res.status(200).json(listaLivros)
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na requisição`})
+        }
+    }
+
+    static async listarLivroPorId(req, res) {
+        try {
+            const id = req.params.id
+            const livroEncontrado = await livro.findById(id)
+            res.status(200).json(livroEncontrado)
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na requisição do livro`})
+        }
     }
 
     static async cadastraLivro (req, res ) {
