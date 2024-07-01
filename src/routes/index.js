a0ace5499e97dac6a43b1a2771dbaf94e5b7f560
Daniel Caresma Silva <daniel.caresma@gmail.com>
2024-07-01 18:06:30 -0300

    Importando rotas de autor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import experss from "express"
 import livros from "./livrosRoutes.js"
+import autores from "./autoresRoutes.js"
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"))
 
     app.use(experss.json(), livros)
+    app.use(experss.json(), autores)
 }
 
 export default routes
